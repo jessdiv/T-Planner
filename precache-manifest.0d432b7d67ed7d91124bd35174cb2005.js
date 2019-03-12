4fd0f1a6447738a6bcb5579ca3461bba975c9993
jessdiv <jessica.diver@hotmail.com>
2019-03-12 17:00:39 +1100

    Updates

---
+++ precache-manifest.0d432b7d67ed7d91124bd35174cb2005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0677ffe02606247df98e",
+    "url": "/project3-syd-ax/static/css/main.6e924f33.chunk.css"
+  },
+  {
+    "revision": "0677ffe02606247df98e",
+    "url": "/project3-syd-ax/static/js/main.0677ffe0.chunk.js"
+  },
+  {
+    "revision": "fe828fbeddf819c84f26",
+    "url": "/project3-syd-ax/static/js/runtime~main.fe828fbe.js"
+  },
+  {
+    "revision": "03e97dd8a199ae66d775",
+    "url": "/project3-syd-ax/static/css/2.7def6b98.chunk.css"
+  },
+  {
+    "revision": "03e97dd8a199ae66d775",
+    "url": "/project3-syd-ax/static/js/2.03e97dd8.chunk.js"
+  },
+  {
+    "revision": "5cef4b13cb27be050bddc8651f7abcd6",
+    "url": "/project3-syd-ax/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Trip-Planner/precache-manifest.cf5a12812d7d34d289145cad05f3e93f.js"
+  "/project3-syd-ax/precache-manifest.0d432b7d67ed7d91124bd35174cb2005.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Trip-Planner/index.html", {
+workbox.routing.registerNavigationRoute("/project3-syd-ax/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
